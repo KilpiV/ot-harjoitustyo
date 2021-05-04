e946b76e251828367be2197bb5984729abeb320f
Kilpivii <viivi.kilpiainen@gmail.com>
2021-05-04 16:36:38 +0300

    uuden luokan testauksen aloitus

--- Helpotus/src/test/java/helpotus/domain/DateCheckingTest.java
+++ Helpotus/src/test/java/helpotus/domain/DateCheckingTest.java
@@ -1,4 +1,3 @@
-
 package helpotus.domain;
 
 import org.junit.After;
@@ -9,26 +8,47 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DateCheckingTest {
-    
-    public DateCheckingTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    DateChecking dates;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        this.dates = new DateChecking();
     }
 
+    @Test
+    public void noticeNoDigits() {
+        String digit = "1234";
+        String notDigit = "1e34";
+        assertTrue(dates.onlyDigits(digit, digit.length()));
+        assertFalse(dates.onlyDigits(notDigit, notDigit.length()));
+    }
+    
+    @Test
+    public void checkYearCorrect() {
+        String year = "2000";
+        String notYear = "20";
+        String notNumber = "year";
+        assertEquals("2000", dates.yearRight(year));
+        assertEquals("Kaikki merkit eivät olleet numeroita", dates.yearRight(notNumber));
+        assertEquals("Vuosiluku ei ole kelvollinen, valitse vuosi väliltä 1990-2100", dates.yearRight(notYear));
+    }
+//    public DateCheckingTest() {
+//    }
+//    
+
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
